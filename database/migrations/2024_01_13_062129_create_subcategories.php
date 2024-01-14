0cf591c7393d7e3f36674aaf3073f6205ecd1f18
Wahyu Kristiawan <whykris97@gmail.com>
2024-01-14 20:17:23 +0700

    Init

---
+++ database/migrations/2024_01_13_062129_create_subcategories.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subcategories', function (Blueprint $table) {
+            $table->integer('id')->unsigned()->autoIncrement();
+            $table->integer('category_id')->unsigned();
+            $table->string('name', 100);
+            $table->text('icon')->nullable();
+
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subcategories');
+    }
+};
